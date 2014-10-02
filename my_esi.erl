a92c486af55246cd76c4d383f17c8c66b8ace343
Ilya Kamens <ilyakamens@gmail.com>
2014-10-02 00:41:29 -0700

    Removed that god awful arrow code... that was so vulgar.

--- my_esi.erl
+++ my_esi.erl
@@ -1,7 +1,7 @@
 % filename: my_esi.erl
 
 -module(my_esi).
--export([start/0, stop/0, testValid/0, hasToken/1, track/3]).
+-export([start/0, stop/0, testValid/0, track/3]).
 -include_lib("inets/include/httpd.hrl"). 
 -import(mochijson, [decode/1]).
 -import(mochijson2, [json_string_is_safe/1]).
@@ -17,91 +17,80 @@ stop() ->
 testValid() ->
 	isValid("data=ew0KICAgICJldmVudCI6ICJTaWduZWQgVXAiLA0KICAgICJwcm9wZXJ0aWVzIjogew0KICAgICAgICAiZGlzdGluY3RfaWQiOiAiMTM3OTMiLA0KICAgICAgICAidG9rZW4iOiAiZTNiYzQxMDAzMzBjMzU3MjI3NDBmYjhjNmY1YWJkZGMiLA0KICAgICAgICAiUmVmZXJyZWQgQnkiOiAiRnJpZW5kIg0KICAgIH0NCn0=").
 
-hasToken(Data) ->
-	case length(Data) /= 0 of
+hasKey(List, Key) when is_list(List) ->
+	case length(List) /= 0 of
 		true ->
-			[Head|Tail] = Data,
-			case element(1, Head) == "token" of
+			[Head|Tail] = List,
+			case hasKey(Head, Key) of
 				true -> true;
-				false -> hasToken(Tail)
+				false -> hasKey(Tail, Key)
+			end;
+		false -> false
+	end;
+hasKey(Tuple, Key) when is_tuple(Tuple) ->
+	tuple_size(Tuple) == 2 andalso element(1, Tuple) == Key;
+hasKey(false, _Key) ->
+	false.
+
+writeToFile(Request) ->
+	file:write_file("./log.txt", io_lib:fwrite("~p.\n\n", [Request]), [append]).
+
+hasValidPrefix(Input) ->
+	string:str(Input, "data=") == 1.
+
+getOutermostTuple(Input) ->
+	Base64Encoded = string:substr(Input, 6),
+	Base64Decoded = base64:decode_to_string(Base64Encoded),
+	mochijson:decode(Base64Decoded).
+
+getOutermostList(OutermostTuple) ->
+	case is_tuple(OutermostTuple) andalso tuple_size(OutermostTuple) == 2 of
+		true -> {_, Request} = OutermostTuple, Request;
+		false -> false
+	end.
+
+getEventTuple(OutermostList) ->
+	case is_list(OutermostList) andalso length(OutermostList) == 2 of
+		true -> [EventTuple|_] = OutermostList, EventTuple;
+		false -> false
+	end.
+
+getPropertiesTuple(OutermostList) ->
+	case is_list(OutermostList) andalso length(OutermostList) == 2 of
+		true ->
+			[_|OuterPropList] = OutermostList,
+			[PropTuple|_] = OuterPropList,
+			PropTuple;
+		false -> false
+	end.
+
+getDataTuple(PropTuple) ->
+	case is_tuple(PropTuple) andalso tuple_size(PropTuple) == 2 andalso element(1, PropTuple) == "properties" of
+		true -> {_, DataTuple} = PropTuple, DataTuple;
+		false -> false
+	end.
+
+getDataList(DataTuple) ->
+	case is_tuple(DataTuple) andalso tuple_size(DataTuple) == 2 of
+		true -> {_, DataList} = DataTuple, DataList;
+		false -> false
+	end.
+
+isValid(Input) ->
+	case hasValidPrefix(Input) of
+		true ->
+			OutermostTuple = getOutermostTuple(Input),
+			OutermostList = getOutermostList(OutermostTuple),
+			EventTuple = getEventTuple(OutermostList),
+			PropertiesTuple = getPropertiesTuple(OutermostList),
+			DataList = getDataList(getDataTuple(PropertiesTuple)),
+			case hasKey(EventTuple, "event") andalso hasKey(PropertiesTuple, "properties") andalso hasKey(DataList, "token") of
+				true -> writeToFile(OutermostTuple), true;
+				false -> false
 			end;
 		false -> false
 	end.
 
-% writeToFile(Request) ->
-% 	file:write_file("./log.txt", io_lib:fwrite("~p.\n\n", [Request]), [append]).
-
-% hasValidPrefix(Input) ->
-% 	string:str(Input, "data=") == 1.
-
-% hasEventParam(EventTuple) ->
-% 	is_tuple(EventTuple) andalso tuple_size(EventTuple) == 2 andalso element(1, EventTuple) == "event";
-
-% getOutermostTuple(Input) ->
-% 	Base64Encoded = string:substr(Input, 6),
-% 	Base64Decoded = base64:decode_to_string(Base64Encoded),
-% 	mochijson:decode(Base64Decoded).
-
-% getOutermostList(OutermostTuple) ->
-% 	case is_tuple(OutermostTuple) andalso tuple_size(OutermostTuple) == 2 of
-% 		true -> {_, Request}, Request;
-% 		false -> false
-% 	end.
-
-% isValid2(Input) ->
-% 	case hasValidPrefix(Input) andalso someFun() of
-% 		true -> Result = "1", write
-
-
-isValid(Input) ->
-	case string:str(Input, "data=") == 1 of
-		true ->
-			Base64Encoded = string:substr(Input, 6),
-			Base64Decoded = base64:decode_to_string(Base64Encoded),
-			% json_string_is_safe doesn't accept valid JSON strings... so I'll just assume it's valid.
-			%case mochijson2:json_string_is_safe(Base64Decoded) of
-				%true ->
-					Tuple = mochijson:decode(Base64Decoded),
-					case is_tuple(Tuple) andalso tuple_size(Tuple) == 2 of
-						true ->
-							{_, Request} = Tuple,
-							case is_list(Request) andalso length(Request) == 2 of
-								true ->
-									[EventTuple|PropTupleArr] = Request,
-									case is_tuple(EventTuple) andalso tuple_size(EventTuple) == 2 andalso element(1, EventTuple) == "event" of
-										true ->
-											[PropTuple|_] = PropTupleArr,
-											case is_tuple(PropTuple) andalso tuple_size(PropTuple) == 2 andalso element(1, PropTuple) == "properties" of
-												true ->
-													{_, Data} = PropTuple,
-													case is_tuple(Data) andalso tuple_size(Data) == 2 of
-														true ->
-															{_, DataArr} = Data,
-															case hasToken(DataArr) of
-																true ->
-																	Result = "1",
-																	file:write_file("./log.txt", io_lib:fwrite("~p.\n\n", [Request]), [append]);
-																false -> Result = "-8"
-															end;
-														false -> Result = "-7"
-													end;
-												false -> Result = "-6"
-											end;
-										false -> Result = "-5"
-									end;
-								false -> Result = "-4"
-							end;
-						false -> Result = "-3"
-					end;
-				%false -> Result = "-2"
-			%end;
-		false -> Result = "-1"
-	end,
-	Result.
-
 track(SessionID, _Env, Input) ->
-	case isValid(Input) == "1" of
-		true -> Result = "1";
-		false -> Result = "0"
-	end,
+	Result = isValid(Input),
 	mod_esi:deliver(SessionID, Result).
